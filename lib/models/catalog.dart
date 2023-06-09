25b270ff8efd015ed59953664dcb701fdb02f4d2
Yash <jainyash0739@gmail.com>
2023-06-09 12:48:45 +0530

    "Day 10 completed"

---
+++ lib/models/catalog.dart
@@ -0,0 +1,27 @@
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image});
+
+}
+
+final products = [
+  Item(
+      id: "Yashpur001",
+      name: "Iphone 12",
+      desc: "Bhai waah waah waah",
+      price: 999,
+      color: "#33505a",
+      image: " "
+  )
+];
