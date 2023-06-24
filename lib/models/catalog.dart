9be7d9c64faf4031f5f1e538933064829d345edf
Yash <jainyash0739@gmail.com>
2023-06-24 14:39:43 +0530

    "Day 13 completed"

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,14 +1,5 @@
 class CatalogModel{
-  static final items = [
-    Item(
-        id: 1,
-        name: "Iphone 12",
-        desc: "Bhai waah waah waah",
-        price: 999,
-        color: "#33505a",
-        image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-  ];
+  static List<Item > items = [];
 }
 
 
@@ -26,6 +17,29 @@ class Item{
     required this.price,
     required this.color,
     required this.image});
+
+  factory Item.fromMap(Map<String,dynamic> map){
+    return Item(
+      id: map['id'],
+      name: map['name'],
+      desc: map['desc'],
+      price: map['price'],
+      color: map['color'],
+      image: map['image'],
+    );
+  }
+
+  toMap() => {
+    "id": id,
+    "name": name,
+    "desc": desc,
+    "price": price,
+    "color": color,
+    "image": image,
+  };
+
+
+
 
 }
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:test_one/models/catalog.dart';
 import 'package:test_one/widgets/drawer.dart';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
 
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  
+  loadData()async{
+    await Future.delayed(Duration(seconds: 2));
+
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productData = decodedData["products"];
+    CatalogModel.items  = List.from(productData).map<Item>((item) =>
+          Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
+
   Widget build(BuildContext context) {
 
     final int days = 77;
     final String name = "Yash";
 
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+
 
     return Scaffold(
       appBar: AppBar(
@@ -22,13 +46,17 @@ class HomePage extends StatelessWidget {
       ),
       body : Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: dummyList.length,
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+            itemCount: CatalogModel.items.length,
             itemBuilder: (context, index){
               return  ItemWidget(
-                item: dummyList[index],
+                item: CatalogModel.items[index],
               );
             }
+        )
+            :Center(
+          child: CircularProgressIndicator(),
         ),
       ),
 
