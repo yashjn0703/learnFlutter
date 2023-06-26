75e1502b9162c02138de555e3f3e66cac037b4db
Yash <jainyash0739@gmail.com>
2023-06-26 13:04:33 +0530

    "Day 15 completed"

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -47,13 +47,42 @@ class _HomePageState extends State<HomePage> {
       body : Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-            itemCount: CatalogModel.items.length,
+            ? GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+            ),
             itemBuilder: (context, index){
-              return  ItemWidget(
-                item: CatalogModel.items[index],
+              final item = CatalogModel.items[index];
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child:  GridTile(
+                    header: Container(
+                      padding: const EdgeInsets.all(12),
+                      child: Text(item.name,
+                      style: TextStyle(color: Colors.white),),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                      ),
+                    ) ,
+                    child: Image.network(item.image),
+                    footer: Container(
+                      padding: const EdgeInsets.all(12),
+                      child: Text(item.price.toString(),
+                        style: TextStyle(color: Colors.white),),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                      ),
+                    ) ,
+                ) ,
               );
-            }
+
+            },
+          itemCount: CatalogModel.items.length,
         )
             :Center(
           child: CircularProgressIndicator(),
