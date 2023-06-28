d99edbdbe29539450c5082abde6145af1adf1370
Yash <jainyash0739@gmail.com>
2023-06-28 16:22:37 +0530

    "Day 18 completed"

---
+++ lib/pages/home_detail_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:test_one/models/catalog.dart';
+import 'package:test_one/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomePageDetail extends StatelessWidget {
+  final Item catalog ;
+  const HomePageDetail({Key? key, required this.catalog})
+      :assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: MyTheme.creamColor,
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceBetween,
+          buttonPadding: EdgeInsets.zero,
+          children: [
+            "\$${catalog.price}".text.bold.xl4.purple700.make(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(backgroundColor:
+              MaterialStateProperty.all(MyTheme.darkBluishColor),
+                  shape: MaterialStateProperty.all(StadiumBorder())),
+              child:"Buy".text.make(),).wh(100, 50)
+          ],
+        ).p32(),
+      ),
+
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Hero(tag: Key(catalog.id.toString()),
+                child: Image.network(catalog.image)).h32(context),
+            Expanded(child: VxArc(
+            height: 30,
+            arcType: VxArcType.CONVEY,
+            edge: VxEdge.TOP,
+            child: Container(
+              color: Colors.white,
+              width: context.screenWidth,
+              child: Column(
+                children: [
+                  catalog.name.text.lg.xl4.color(MyTheme.darkBluishColor).bold.make(),
+                  catalog.desc.text.xl.textStyle(context.captionStyle).make(),
+                  10.heightBox,
+                ],
+              ).py64(),
+            ),
+            )
+            )
+          ],
+        ),
+      )
+
+    );
+  }
+}
